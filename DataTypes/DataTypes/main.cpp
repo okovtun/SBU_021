27a984bad5387d06715e1cad110ccc934781d2ac
Oleg <31523835+okovtun@users.noreply.github.com>
2020-09-12 12:29:08 +0300

    DataTypes in progress

---
+++ DataTypes/DataTypes/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "Russian");
+	//cout << false << endl;
+	//cout << (bool)0.0001 << endl;
+
+	cout << sizeof(short) << endl;
+	cout << "\n---------------------------------------\n";
+}
