608d83298273ca571229053f956f97a61923e0d0
Clayman <Clayman@DESKTOP-649SK3N>
2020-09-26 21:57:01 +0300

    Control structures start.

---
+++ ControlStructures/ControlStructures/main.cpp
@@ -0,0 +1,27 @@
+//ControlStructures
+#include<iostream>
+using namespace std;
+//FP - Fundamentals Programming
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+	int temperature;
+	cout << "Введите температуру воздуха: "; cin >> temperature;
+	cout << temperature << endl;
+	if (temperature > 15)
+	{
+		cout << "На улице тепло :-)" << endl;
+	}
+	else
+	{
+		cout << "На улице холодно :-(" << endl;
+	}
+	if (Condition)
+	{
+
+	}
+	else
+	{
+
+	}
+}
