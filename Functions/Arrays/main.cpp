53dfecdc4da0e5f717d6c64703064f81b21e79b9
Clayman <Clayman@DESKTOP-649SK3N>
2020-12-05 11:40:12 +0200

    Function overloading

--- Functions/Arrays/main.cpp
+++ Functions/Arrays/main.cpp
@@ -1,20 +1,40 @@
 #include<iostream>
 using namespace std;
 
+const int ROWS = 8;
+const int COLS = 5;
+
 void FillRand(int arr[], const int n);
+void FillRand(double arr[], const int n);
+void FillRand(int arr[ROWS][COLS], const int m, const int n);
+void FillRand(double arr[ROWS][COLS], const int m, const int n);
+
 void Print(int arr[], const int n);
+void Print(double arr[], const int n);
+void Print(int arr[ROWS][COLS], const int m, const int n);
+void Print(double arr[ROWS][COLS], const int m, const int n);
 
 void main()
 {
 	setlocale(LC_ALL, "");
+	
 	const int n = 5;
 	int arr[n];
 	FillRand(arr, n);
 	Print(arr, n);
 
-	int brr[n];
+	double brr[n];
 	FillRand(brr, n);
 	Print(brr, n);
+
+	
+	int arr2[ROWS][COLS];
+	FillRand(arr2, ROWS, COLS);
+	Print(arr2, ROWS, COLS);
+
+	double brr2[ROWS][COLS];
+	FillRand(brr2, ROWS, COLS);
+	Print(brr2, ROWS, COLS);
 }
 
 void FillRand(int arr[], const int n)
@@ -24,6 +44,36 @@ void FillRand(int arr[], const int n)
 		arr[i] = rand() % 100;
 	}
 }
+void FillRand(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 10000;
+		arr[i] /= 100;
+	}
+}
+void FillRand(int arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+void FillRand(double arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			arr[i][j] = rand() % 10000;
+			arr[i][j] /= 100;
+		}
+	}
+}
+
 
 void Print(int arr[], const int n)
 {
@@ -32,4 +82,34 @@ void Print(int arr[], const int n)
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
+}
+void Print(double arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(int arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
+void Print(double arr[ROWS][COLS], const int m, const int n)
+{
+	for (int i = 0; i < m; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << endl;
+	}
 }
