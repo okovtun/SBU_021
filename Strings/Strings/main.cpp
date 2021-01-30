ff4548661acea5154cb5575ce1842f8b186f50ea
Clayman <Clayman@DESKTOP-DM8E77U>
2021-01-30 13:43:43 +0200

    Strings day 1

---
+++ Strings/Strings/main.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+void InputLine(char str[], const int n);
+int StrLen(char str[]);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	'\0';	//ASCII-символ с кодом 0
+	//Null terminated lines
+	//char str[6] = { 'H', 'e', 'l', 'l', 'o', '\0' };
+	//for (int i = 0; i < 5; i++)cout << str[i]; cout << endl;
+	//cout << str << endl;
+	/*char str[] = "Hello";
+	cout << str << endl;*/
+
+	/*system("CHCP 1251");
+	system("CLS");*/
+
+	const int n = 20;
+	char str[n];
+	cout << "Введите строку: ";
+	InputLine(str, n);
+	cout << str << endl;
+	cout << "Длина строки: " << StrLen(str) << endl;
+}
+
+void InputLine(char str[], const int n)
+{
+	SetConsoleCP(1251);
+	//cin >> str;
+	cin.getline(str, n);
+	SetConsoleCP(866);
+}
+
+int StrLen(char str[])
+{
+	//Возвращает длину строки
+	int i = 0;
+	for (; str[i]; i++);
+	return i;
+}
